82fb5ef97b8bc4612800d0a8c82654533849a65f
Guadalberto <99971879+Guadalberto@users.noreply.github.com>
2022-02-21 09:09:34 -0500

    s

--- BS4/scratch_3.py
+++ BS4/scratch_3.py
@@ -1,4 +1,8 @@
 from bs4 import BeautifulSoup
-with open("index.html", "r") as f:
+
+with open("index.html", "rb") as f:
     doc = BeautifulSoup(f, "html.parser")
-print(doc)
+#print(doc.prettify())
+
+tag = doc.title
+print(tag)
